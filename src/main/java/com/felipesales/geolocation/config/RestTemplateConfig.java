c4c095ab28ebee35f2744be05d81d54a45b2a8b2
felipetop <top_cs@hotmail.com>
2019-08-29 19:57:03 -0300

    Was added swagger on project

--- src/main/java/com/felipesales/geolocation/config/RestTemplateConfig.java
+++ src/main/java/com/felipesales/geolocation/config/RestTemplateConfig.java
@@ -1,4 +1,4 @@
-package com.felipesales.geolocation.service;
+package com.felipesales.geolocation.config;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
--- src/main/java/com/felipesales/geolocation/controller/ClientController.java
+++ src/main/java/com/felipesales/geolocation/controller/ClientController.java
@@ -2,6 +2,7 @@ package com.felipesales.geolocation.controller;
 
 import com.felipesales.geolocation.model.Client;
 import com.felipesales.geolocation.service.ClientService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -12,31 +13,37 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/clients")
+@CrossOrigin(origins = "*")
 public class ClientController {
 
     @Autowired
     private ClientService clientService;
 
+    @ApiOperation(value = "Deleta um Cliente, a partir do identificador do Cliente")
     @DeleteMapping("/{id}")
     public void delete(@PathVariable("id") Integer id) {
         clientService.delete(id);
     }
 
+    @ApiOperation(value = "Retorna todos os Clientes")
     @GetMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public List<Client> findAll() {
         return clientService.findAll();
     }
 
+    @ApiOperation(value = "Retorna o Cliente a partir do indentificador do Cliente")
     @GetMapping("/{id}")
     public Client findById(@PathVariable("id") Integer id) {
         return clientService.findById(id);
     }
 
+    @ApiOperation(value = "Cria um novo Cliente")
     @PostMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Client save(@RequestBody @Valid Client client, HttpServletRequest request) {
         return clientService.save(client, request);
     }
 
+    @ApiOperation(value = "Altera um Cliente")
     @PutMapping("/{id}")
     public Client update(@PathVariable("id") Integer id, @RequestBody @Valid Client client) {
         return clientService.update(id, client);
